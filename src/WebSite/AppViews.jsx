cf7faf15ecb9448c21c7e9d6abafd3e7f85ebc26
Mike Sheets <wmichaelsheets@gmx.com>
2024-09-23 15:16:40 -0400

    routes, etc.

---
+++ src/WebSite/AppViews.jsx
@@ -0,0 +1,17 @@
+import { Outlet, Route, Routes } from "react-router-dom"
+import { NavBar } from "./NavBar"
+
+export const AppViews = () => {
+    return(
+        <Routes>
+    <Route path="/" element={ <>
+        <NavBar/>
+        <Outlet />
+    </> }>
+        <Route path="/nurseries" element={ <div>Text</div> } />
+        <Route path="/distributors" element={ <></> } />
+        <Route path="/retailers" element={ <></> } />
+    </Route>
+</Routes>
+    )
+}
