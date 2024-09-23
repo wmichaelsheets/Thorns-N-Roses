e7a7fbe96e4dd3a3fd7c8f01eb334288dcbc3ee1
Mike Sheets <wmichaelsheets@gmx.com>
2024-09-23 16:31:54 -0400

    updated NavBar

---
+++ src/Services/NurseryService.jsx
@@ -0,0 +1,6 @@
+export const getFlowerByNursery = () => {
+    return fetch('http://localhost:8088/nurseries?_embed=flowersJoin').then((res) =>
+        res.json()
+    )
+}
+
--- src/WebSite/NavBar.jsx
+++ src/WebSite/NavBar.jsx
@@ -3,16 +3,18 @@ import "./NavBar.css"
 
 export const NavBar = () => {
     return (
-        <ul className='nav-bar'>
-            <li>
-                <Link to={'/nurseries'}>Nurseries</Link>
-            </li>
-            <li>
-                <Link to={'/distributors'}>Distributors</Link>
-            </li>
-            <li>
-                <Link to={'/retailers'}>Retailers</Link>
-            </li>
-        </ul>
+        <div className="nav-bar-container">
+            <ul className='nav-bar'>
+                <li>
+                    <Link to={'/nurseries'}>Nurseries</Link>
+                </li>
+                <li>
+                    <Link to={'/distributors'}>Distributors</Link>
+                </li>
+                <li>
+                    <Link to={'/retailers'}>Retailers</Link>
+                </li>
+            </ul>
+        </div>
     )
 }
