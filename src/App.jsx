5363e426d7fd602ca88fc52d4c3440813c31fa24
Dustin Potter <dustinrp1@gmail.com>
2024-09-25 14:41:24 -0500

    Added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css';
 import { AppViews } from './WebSite/AppViews';
 
+import React from 'react';
+import './App.css';
+
 function App() {
-  return <AppViews />;
+  return (
+    <div>
+      <div className="margins left"></div>
+      <div className="margins right"></div>
+      <div id="root">
+      <AppViews />;
+      </div>
+    </div>
+  );
 }
 
 export default App;
